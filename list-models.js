f5c3b06639d95b694c9977e01eb64efa2b954d49
Omdarshan Shindepatil <omdarshanpatil@gmail.com>
2025-11-10 14:28:30 +0530

    chore: ignore local test scripts

--- list-models.js
+++
@@ -1,9 +0,0 @@
-import fetch from "node-fetch";
-
-const key = process.env.GEMINI_API_KEY || "AIzaSyCDo_WCBU5HoxV-gRX_jRQtYyGEI6VHPuk";
-
-const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${key}`);
-const data = await res.json();
-
-console.log("✅ Available models:");
-data.models.forEach((m) => console.log(m.name));
